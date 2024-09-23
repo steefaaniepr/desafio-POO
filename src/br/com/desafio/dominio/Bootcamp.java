9dd90d620d966ba34f2e88811f25ae8593b1bf1d
Stefanie Paes <steefaaniepr@outlook.com>
2024-09-23 00:32:59 -0300

    Criando DEV e BOOTCAMP

---
+++ src/br/com/desafio/dominio/Bootcamp.java
@@ -0,0 +1,5 @@
+package br.com.desafio.dominio;
+
+public class Bootcamp {
+    
+}
---
+++ src/br/com/desafio/dominio/Dev.java
@@ -0,0 +1,13 @@
+package br.com.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    
+    private String nome;
+    private Set<Conteudo> conteudosIscritos = new LinkedHashSet<>();
+    private Set<Conteudo> ConteudosConcluidos = new LinkedHashSet<>(); 
+    
+
+}
