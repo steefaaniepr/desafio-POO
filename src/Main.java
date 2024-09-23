df68b1a43294132059a4cc9d22222e5373cca8b7
Stefanie Paes <steefaaniepr@outlook.com>
2024-09-23 00:06:38 -0300

    Criando atributos e metodos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
+import java.time.LocalDate;
+
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
 public class Main {
     
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição Curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição de Mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
 }
--- src/br/com/desafio/dominio/Mentoria.java
+++ src/br/com/desafio/dominio/Mentoria.java
@@ -4,8 +4,33 @@ import java.time.LocalDate;
 
 public class Mentoria {
 
-    String titulo;
-    String descricao;
-    LocalDate data;
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+    }
     
 }
